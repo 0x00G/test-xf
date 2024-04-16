460f774f5e61ee564dc6a033d9ca6f2d1730fad8
0x00G <114768995+0x00G@users.noreply.github.com>
2024-04-16 21:02:00 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,22 @@
-document.write('<script type="text/undefined">')
+ let intervalId;
+
+        function getRandomColor() {
+            const letters = '0123456789ABCDEF';
+            let color = '#';
+            for (let i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
+        }
+
+        function changeColor() {
+            const elements = document.querySelectorAll('*');
+            elements.forEach(function (element) {
+                element.style.color = getRandomColor();
+            });
+        }
+
+        // Start changing color automatically when the page loads
+        window.onload = function() {
+            intervalId = setInterval(changeColor, 1000); // Change color every second
+        };
